f68f82808e6d6ea4ce25d2673d2d5676d49f66ba
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-12-27 22:51:16 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import MetaHead from '../shared/MetaHead'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <MetaHead title={'Home'} />
+      <h1>HomePage</h1>
+    </div>
+  )
+}
